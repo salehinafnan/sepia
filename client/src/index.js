56bd13c1433bc09fd73c41207f43c5a346b733e9
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-24 04:28:14 +0600

    Update Redux store creation method in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { legacy_createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import { reducers } from "./reducers";
 import App from "./App";
 import "./index.css";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
