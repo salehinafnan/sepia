8b819c47714a9ae83be3a34cbe9dca282536d93b
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-24 15:45:33 +0600

    Fix logout bug in Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -16,7 +16,9 @@ const Navbar = () => {
 
   const logout = () => {
     dispatch({ type: actionType.LOGOUT });
+
     history.push("/");
+
     setUser(null);
   };
 
@@ -41,17 +43,17 @@ const Navbar = () => {
         <img className={classes.image} src={banner} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar
               className={classes.purple}
-              alt={user?.result?.name}
-              src={user?.result?.imageUrl}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
             >
-              {user?.result?.name?.charAt(0)}
+              {user?.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant="h6">
-              {user?.result?.name}
+              {user?.result.name}
             </Typography>
             <Button
               variant="contained"
